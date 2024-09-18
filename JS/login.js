1a3fa8848a7bddec1877474a795a12056d71bd77
jyotiprokash <jyotiprokash2000@gmail.com>
2024-09-19 02:38:34 +0600

    added log in form

---
+++ JS/login.js
@@ -0,0 +1,15 @@
+// login form
+
+document.getElementById('button').addEventListener("click", 
+function(event){
+    event.preventDefault();
+    const phoneNumber=document.getElementById('phone-number').value;
+    const pinNumber=document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber)
+    if(phoneNumber==='01746153493' && pinNumber==='1234'){
+        alert("you have log in success");
+    }
+    else{
+            alert('wrong number and password');
+    }
+})
